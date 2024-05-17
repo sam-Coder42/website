08ec6664040eecce9bebaa45e059dd7c7a4594df
sam-Coder42 <165879277+sam-Coder42@users.noreply.github.com>
2024-05-17 17:36:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+     entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+     });
+});
+
+
+
+const hiddenElements = document.querySelectorAll('main-section');
+hiddenElements.forEach((el) => observer.observe(el));
